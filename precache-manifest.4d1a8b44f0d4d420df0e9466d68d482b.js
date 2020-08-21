bc6f60cea0aa5fa54100e58c849bf30266a4d0e0
explooosion <ta7382@gmail.com>
2020-08-21 06:49:54 +0000

    Deploying to gh-pages from master 00c57d7b83bcfe0896e97490bcf0758d421962bc

---
+++ precache-manifest.4d1a8b44f0d4d420df0e9466d68d482b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8532adccdf5655c616b22f92d36f548b",
+    "url": "/dailydrinks/index.html"
+  },
+  {
+    "revision": "011acc3d96cac1cac13b",
+    "url": "/dailydrinks/static/js/2.6baa607e.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/dailydrinks/static/js/2.6baa607e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a3b67945fa6d68ffbd1",
+    "url": "/dailydrinks/static/js/main.fb12a18e.chunk.js"
+  },
+  {
+    "revision": "ac03f82b3f5aff6b010d",
+    "url": "/dailydrinks/static/js/runtime-main.c6fe6444.js"
+  }
+]);
